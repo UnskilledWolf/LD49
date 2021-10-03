81dd4aecaa6990b74791313fe3205feb8c7f4852
Unskilled Wolf <geronimo.xl13@gmail.com>
2021-10-03 17:20:49 +0200

    Voice Lines

--- src/scripts/ReactorAutoRepair.gd
+++ src/scripts/ReactorAutoRepair.gd
@@ -2,8 +2,27 @@ extends Node
 
 export var repait_ammount:int=5;
 
+var played1min = false;
+var played15sec = false;
+var played30sec = false;
+var playedarrival = false;
+
 func _ready():
 	GameManager.countdown_timer = $GameEndTimer
+
+func _process(delta):
+	if not played1min and GameManager.countdown_timer.time_left <= 60:
+		played1min = true
+		$An1Min.play()
+	elif not played30sec and GameManager.countdown_timer.time_left <= 30:
+		played30sec = true
+		$An30Sec.play()
+	elif not played15sec and GameManager.countdown_timer.time_left <= 15:
+		played15sec = true
+		$An15Sec.play()
+	elif not playedarrival and GameManager.countdown_timer.time_left <= 6:
+		playedarrival = true
+		$Arrival.play()
 
 func _on_Timer_timeout():
 	GameManager.remove_reactor_stability(-repait_ammount)
