b1a1e640d7dec6462c3c39c5664572ebf24a8aee
Unskilled Wolf <geronimo.xl13@gmail.com>
2021-10-03 12:39:04 +0200

    Updated Graphics

--- src/scripts/ReactorInteract.gd
+++ src/scripts/ReactorInteract.gd
@@ -1,6 +1,7 @@
 extends StaticBody2D
 
 func interact():
-	$DropParticles.texture = GameManager.player_node.tmp_item_texture
-	$DropParticles.restart()
-	GameManager.player_node.remove_item()
+	if GameManager.player_node.is_holding_item:
+		$DropParticles.texture = GameManager.player_node.tmp_item_texture
+		$DropParticles.restart()
+		GameManager.player_node.remove_item()
