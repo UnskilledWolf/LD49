9db7667d9aeeeb82c29306d5374585d40c86a09a
Unskilled Wolf <geronimo.xl13@gmail.com>
2021-10-03 12:53:52 +0200

    2d lights

--- src/scripts/Screen.gd
+++ src/scripts/Screen.gd
@@ -8,6 +8,7 @@ var danger_state = false
 
 func _ready():
 	$Screen_Inner.modulate = normal_color
+	$Light.color = normal_color
 
 func _process(delta):
 	$Control/VBoxContainer/StabilityBar.value = GameManager.reactor_stability
@@ -16,6 +17,8 @@ func _process(delta):
 	if not danger_state and GameManager.reactor_stability < danger_threshhold:
 		danger_state = true
 		$Screen_Inner.modulate = danger_color
+		$Light.color = danger_color
 	elif danger_state and GameManager.reactor_stability > danger_threshhold:
 		danger_state = false
 		$Screen_Inner.modulate = normal_color
+		$Light.color = normal_color
